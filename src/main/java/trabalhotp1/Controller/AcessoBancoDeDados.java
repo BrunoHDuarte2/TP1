35819abe44615ce78dcb4be4fa4d35d7a98efa19
bdebatata <bdebatatadoce@gmail.com>
2025-01-02 14:08:01 -0300

    Design inicial

---
+++ src/main/java/trabalhotp1/Controller/AcessoBancoDeDados.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabalhotp1.Controller;
+
+import java.util.ArrayList;
+import trabalhotp1.Model.Equipamento;
+import trabalhotp1.Model.Funcionario;
+
+/**
+ *
+ * @author bhdbr
+ */
+public class AcessoBancoDeDados {
+    private ArrayList<Funcionario> funcionarios = new ArrayList();
+    private ArrayList<Equipamento> equipamentos = new ArrayList();
+    public AcessoBancoDeDados(){
+        // Por enquanto só simulado
+    }
+}
--- src/main/java/trabalhotp1/View/TelaGeral.java
+++ src/main/java/trabalhotp1/View/TelaGeral.java
@@ -1,5 +1,8 @@
 package trabalhotp1.View;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
@@ -9,12 +12,12 @@ package trabalhotp1.View;
  *
  * @author bhdbr
  */
-public class TelaInicial extends javax.swing.JFrame {
-
+public class TelaGeral extends javax.swing.JFrame {
+    
     /**
      * Creates new form TelaInicial
      */
-    public TelaInicial() {
+    public TelaGeral() {
         initComponents();
     }
 
@@ -27,21 +30,53 @@ public class TelaInicial extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        funcButton = new javax.swing.JButton();
+        equiButton = new javax.swing.JButton();
+        manButton = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        funcButton.setText("Funcionários");
+
+        equiButton.setText("Equipamentos");
+
+        manButton.setText("Manutenções");
+        manButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                manButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1000, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(250, Short.MAX_VALUE)
+                .addComponent(funcButton)
+                .addGap(150, 150, 150)
+                .addComponent(manButton)
+                .addGap(150, 150, 150)
+                .addComponent(equiButton)
+                .addContainerGap(250, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 472, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(320, 320, 320)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(manButton)
+                    .addComponent(equiButton)
+                    .addComponent(funcButton))
+                .addContainerGap(130, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void manButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_manButtonActionPerformed
+        new TelaManuntencao().setVisible(true);
+    }//GEN-LAST:event_manButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -60,24 +95,28 @@ public class TelaInicial extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(TelaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaGeral.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(TelaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaGeral.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(TelaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaGeral.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(TelaInicial.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(TelaGeral.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new TelaInicial().setVisible(true);
+                new TelaGeral().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton equiButton;
+    private javax.swing.JButton funcButton;
+    private javax.swing.JButton manButton;
     // End of variables declaration//GEN-END:variables
 }
