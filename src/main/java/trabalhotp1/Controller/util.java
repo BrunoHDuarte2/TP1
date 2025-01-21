33bfbd1abd06d366f16fd7c582b14a757064e3ff
bdebatata <bdebatatadoce@gmail.com>
2025-01-21 11:30:29 -0300

    Tem que arrumar a interface

---
+++ src/main/java/trabalhotp1/Controller/util.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package trabalhotp1.Controller;
+
+/**
+ *
+ * @author bhdbr
+ */
+public interface util {
+    public void salvarListaManutencoes();
+}
