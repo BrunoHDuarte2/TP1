4b6184da14e47030a7dbdabda524d721a0ac1c7c
bdebatata <bdebatatadoce@gmail.com>
2025-01-26 21:03:12 -0300

    Subindo a versão estável anterior

--- src/main/java/trabalhotp1/Controller/Main.java
+++ src/main/java/trabalhotp1/Controller/Main.java
@@ -19,11 +19,6 @@ import trabalhotp1.Model.Funcionario;
 public class Main {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
         
-        AcessoBancoDeDados banco = new AcessoBancoDeDados();
-        banco.criarEquipamento(new Eletrico("Compressor", banco.getEquipamento().size(), "AF-12", Estado.AGUARDANDO_MANUTENCAO));
-        banco.criarEquipamento(new Eletronico("Bomba", banco.getEquipamento().size(), "AF-12", Estado.AGUARDANDO_MANUTENCAO));
-        banco.criarFuncionario(new Funcionario("1324", "Bruno", new Date(), "bdebatata", "1234", "Engenharia", Especialidade.Eletrônica));
-        banco.criarFuncionario(new Funcionario("1325", "David", new Date(), "deidi", "5678", "Industria", Especialidade.Elétrica));
        
     }
 }
