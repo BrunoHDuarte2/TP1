db6b5283c3e777ab1cfad2ee36f2928bb1ec00d8
brunotarquinio <107733709+brunotarquinio@users.noreply.github.com>
2025-02-06 20:43:06 -0300

    Update Especialidade.java

--- src/main/java/trabalhotp1/Model/Especialidade.java
+++ src/main/java/trabalhotp1/Model/Especialidade.java
@@ -6,14 +6,11 @@ package trabalhotp1.Model;
 
 /**
  *
- * @author bhdbr/brunosilva
+ * @author bhdbr
  */
 public enum Especialidade {
     Eletrônica,
     Elétrica,
     Química;
 
-    public boolean contains(Especialidade especialidade) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
 }
